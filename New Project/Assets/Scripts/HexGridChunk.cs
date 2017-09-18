f77de512b2b29c95c7082ff1bcb5fcc313609b01
dddkdk4521 <dddkdk@naver.com>
2017-09-18 17:51:56 +0900

    Update New Project
    2017-09-18

--- New Project/Assets/Scripts/HexGridChunk.cs
+++ New Project/Assets/Scripts/HexGridChunk.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class HexGridChunk : MonoBehaviour {
-
+public class HexGridChunk : MonoBehaviour
+{
 	public HexMesh terrain, rivers, roads, water, waterShore, estuaries;
 
 	public HexFeatureManager features;
@@ -62,8 +62,19 @@ public class HexGridChunk : MonoBehaviour {
 		for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++) {
 			Triangulate(d, cell);
 		}
-		if (!cell.IsUnderwater && !cell.HasRiver && !cell.HasRoads) {
-			features.AddFeature(cell, cell.Position);
+
+        // Avoiding Water
+		if (!cell.IsUnderwater)
+        {
+			if (!cell.HasRiver && !cell.HasRoads)
+            {
+				features.AddFeature(cell, cell.Position);
+			}
+
+            if (cell.IsSpecial)
+            {
+				features.AddSpecialFeature(cell, cell.Position);
+			}
 		}
 	}
 
@@ -357,6 +368,12 @@ public class HexGridChunk : MonoBehaviour {
 				corner = HexMetrics.GetFirstSolidCorner(direction);
 			}
 			roadCenter += corner * 0.5f;
+			if (cell.IncomingRiver == direction.Next() && (
+				cell.HasRoadThroughEdge(direction.Next2()) ||
+				cell.HasRoadThroughEdge(direction.Opposite())
+			)) {
+				features.AddBridge(roadCenter, center - corner * 0.5f);
+			}
 			center += corner * 0.25f;
 		}
 		else if (cell.IncomingRiver == cell.OutgoingRiver.Previous()) {
@@ -392,7 +409,17 @@ public class HexGridChunk : MonoBehaviour {
 			) {
 				return;
 			}
-			roadCenter += HexMetrics.GetSolidEdgeMiddle(middle) * 0.25f;
+			Vector3 offset = HexMetrics.GetSolidEdgeMiddle(middle);
+			roadCenter += offset * 0.25f;
+			if (
+				direction == middle &&
+				cell.HasRoadThroughEdge(direction.Opposite())
+			) {
+				features.AddBridge(
+					roadCenter,
+					center - offset * (HexMetrics.innerToOuter * 0.7f)
+				);
+			}
 		}
 
 		Vector3 mL = Vector3.Lerp(roadCenter, e.v1, interpolators.x);
